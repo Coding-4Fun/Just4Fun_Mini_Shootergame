c96207a9c46fc70d88b811a1404736629266932d
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-02-16 20:37:22 +0100

    Add: Timer zum automatischen Despawn eines Dummys mit Minus Punkte bei Timeout

--- DummyTarget/DummyTarget.gd
+++ DummyTarget/DummyTarget.gd
@@ -3,23 +3,30 @@ extends StaticBody2D
 signal Hit
 
 export var score : int = 1
+export var autoDespawn = true
 
 onready var Main = get_tree().get_root().get_node("MainGame")
 
 
 # Called when the node enters the scene tree for the first time.
-#func _ready() -> void:
-#	pass # Replace with function body.
-
-
-#func _exit_tree() -> void:
-#	print("Dummy exit Tree")
-#	pass
+func _ready() -> void:
+	if autoDespawn :
+		$despawnTimer.start()
 
 
 func _hit_ByBall():
 	emit_signal("Hit", score)
 	queue_free()
+
+
+func _on_despawnTimer_timeout() -> void:
+	emit_signal("Hit", score*-1)
+	queue_free()
+
+
+#func _exit_tree() -> void:
+#	print("Dummy exit Tree")
+#	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
