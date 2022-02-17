fd9a777dcd5eac56ef4c09ef37b8073208a6e684
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-02-17 13:14:10 +0100

    Upd: Anpassung an UI ===: Mausbehandlung Add: Panel für Einstellungen die bei einem Reset wirksam werden und den Spielverlauf anpassen Add: Button zum ein- / Ausblenden der Einstellungen

--- UI/InGameUI.gd
+++ UI/InGameUI.gd
@@ -7,34 +7,41 @@ var _score : int = 0
 
 signal UIResetGame
 
+onready var OptionUI = $IngameUIBottom/vBoxContainer/hBoxOptions
+
 func _ready() -> void:
-	$IngameUIBottom/vBoxContainer/hBoxShots/labShots.text = str(_shots)
-	$IngameUIBottom/vBoxContainer/hBoxScore/labScore.text = str(_score)
+	$IngameUIBottom/vBoxContainer/hBoxHud/hBoxShots/labShots.text = str(_shots)
+	$IngameUIBottom/vBoxContainer/hBoxHud/hBoxScore/labScore.text = str(_score)
 
 
 # Update UI Label Text when the shootpower changed
 func _on_Cannon_CannonAngelChange(newPower : int) -> void:
-	$IngameUIBottom/vBoxContainer/hBoxAngel/labAngel.text = str(newPower)
+	$IngameUIBottom/vBoxContainer/hBoxHud/hBoxAngel/labAngel.text = str(newPower)
 
 
 # Update UI Label when the shootangel changed
 func _on_Cannon_CannonPowerChange(newAngel : int) -> void:
-	$IngameUIBottom/vBoxContainer/hBoxPower/labPower.text = str(newAngel)
+	$IngameUIBottom/vBoxContainer/hBoxHud/hBoxPower/labPower.text = str(newAngel)
 
 
 func _on_Cannon_Shot() -> void:
 	_shots += 1
-	$IngameUIBottom/vBoxContainer/hBoxShots/labShots.text = str(_shots)
+	$IngameUIBottom/vBoxContainer/hBoxHud/hBoxShots/labShots.text = str(_shots)
 
 
 func _on_UIScore_Change(score) -> void:
 	_score += score
-	$IngameUIBottom/vBoxContainer/hBoxScore/labScore.text = str(_score)
+	$IngameUIBottom/vBoxContainer/hBoxHud/hBoxScore/labScore.text = str(_score)
 
 
 func _on_button_pressed() -> void:
 	_shots = 0
 	_score = 0
-	$IngameUIBottom/vBoxContainer/hBoxScore/labScore.text = str(_score)
-	$IngameUIBottom/vBoxContainer/hBoxShots/labShots.text = str(_shots)
+	$IngameUIBottom/vBoxContainer/hBoxHud/hBoxScore/labScore.text = str(_score)
+	$IngameUIBottom/vBoxContainer/hBoxHud/hBoxShots/labShots.text = str(_shots)
 	emit_signal("UIResetGame")
+
+
+func _on_buttGameOptions_pressed() -> void:
+	OptionUI.visible = !OptionUI.visible
+	pass # Replace with function body.
