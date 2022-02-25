2a01bc5bef43037e58d3a86613ea329b280f11af
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-02-25 21:42:11 +0100

    Refac: func add_DummyTarget ===: Angepasst an Tilemap Coordinaten

--- MainGame.gd
+++ MainGame.gd
@@ -27,6 +27,7 @@ onready var cannonBarrelLeftMuzzle = cannonBarrelLeft.get_node("Muzzle")
 
 onready var UIMain = $InGameUI
 onready var line = $TrajectoryLine
+onready var Map = $TerrainMap
 
 var Explosion = preload("res://CannonBall/Explosion.tscn")
 var Dummy = preload("res://DummyTarget/DummyTarget.tscn")
@@ -45,7 +46,7 @@ func _ready():
 	if (tmp) != OK:
 		print("Connect MainGame::_ready() -> Connect to CannonReset Fehlgeschlagen: %s" % tmp)
 
-	$TerrainMap.generate_world_tilemap_base()
+	Map.generate_world_tilemap_base()
 #	$Chunk.generate_world_block_base()
 
 
@@ -53,15 +54,15 @@ func add_cannon_left(pos:Vector2 = Vector2.INF):
 #	cannonLeft.position = TerrainLine.points[0]
 #	cannonLeft.position.x = TerrainLine.castlewidth / 2
 	if pos == Vector2.INF:
-		var p = $TerrainMap.plattform[ceil($TerrainMap.plattform.size()/2)]
-		cannonLeft.position = $TerrainMap.map_to_world(p)
+		var p = Map.plattform[ceil($TerrainMap.plattform.size()/2)]
+		cannonLeft.position = Map.map_to_world(p)
 #		cannonLeft.position.x = Chunk.castlewidth / 2
 		PlayerLeft.add_child(cannonLeft)
 	else:
 		cannonLeft.position = pos
 
 
-func add_DummyTarget():
+func add_DummyTarget_to_Line():
 	if Chunk.points.size() < 5:
 #	if TerrainLine.points.size() < 5:
 		pass
@@ -87,9 +88,31 @@ func add_DummyTarget():
 	call_deferred("add_child", target)
 
 
+func add_DummyTarget():
+	randomize()
+
+	var rand = ceil(rand_range(Map.mod, Map.world_tiles_x))
+	var tiles = Map.get_used_cells_by_id(0)
+
+	var randtile = ceil(rand_range(0, tiles.size()-1))
+
+	target = Dummy.instance()
+	var tile_coord = tiles[ceil(randtile)]
+	var posi = Map.map_to_world(tile_coord)
+	target.position = posi
+	target.score += int(randtile)
+#	target.get_transform().scaled(Vector2(1,1))
+	target.autoDespawn = Config.config_data["Game"]["DummyTarget"]["TimerEnabled"]
+
+	if !target.is_connected("Hit", self, "_on_Dummy_Hited"):
+		target.connect("Hit", self, "_on_Dummy_Hited")
+
+	call_deferred("add_child", target)
+
+
 func _on_MainGame_ready() -> void:
 	add_cannon_left()
-#	add_DummyTarget()
+	add_DummyTarget()
 	pass
 
 
