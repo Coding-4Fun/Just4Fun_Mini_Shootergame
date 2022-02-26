e861c999db651303be399ff3867d7e0e5c43c80d
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-02-26 23:35:42 +0100

    Rem: Nicht benötigte onready Variablen Rem: onready Variablen die im Global vorhanden sind Upd: Aufrufe an Globals und Signals angepasst Rem: Func AddTargetToLine und andere Line Funcs ===: Nicht mehr benötigt, seit TileMap

--- MainGame.gd
+++ MainGame.gd
@@ -4,20 +4,7 @@ signal UIScoreChange
 signal CannonReset
 
 
-#onready var TerrainLine = $"TerrainLine"
-onready var Chunk = $"Chunk"
-
-onready var Players = $Players
-onready var PlayerLeft = $Players/Player1
-onready var PlayerRight = $Players/Player2
-
-#onready var castle = preload("res://Castle2d.tscn")
-#onready var castleleft = castle.instance()
-#onready var castleright = castle.instance()
-
-onready var cannon = preload("res://Cannon/Cannon.tscn")
-
-onready var cannonLeft = cannon.instance()
+onready var cannonLeft = Preloads.Cannon.instance()
 onready var cannonBarrelLeft = cannonLeft.get_node("Barrel")
 onready var cannonBarrelLeftMuzzle = cannonBarrelLeft.get_node("Muzzle")
 
@@ -25,80 +12,58 @@ onready var cannonBarrelLeftMuzzle = cannonBarrelLeft.get_node("Muzzle")
 #onready var cannonBarrelRight = cannonRight.get_node("Barrel")
 #onready var cannonBarrelRightMuzzle = cannonBarrelRight.get_node("Muzzle")
 
-onready var UIMain = $InGameUI
-onready var line = $TrajectoryLine
-onready var Map = $TerrainMap
 
-var Explosion = preload("res://CannonBall/Explosion.tscn")
-var Dummy = preload("res://DummyTarget/DummyTarget.tscn")
+
+
 var target
 
 
 func _ready():
-	cannonLeft.connect("CannonAngelChange", UIMain, "_on_Cannon_CannonAngelChange")
-	cannonLeft.connect("CannonPowerChange", UIMain, "_on_Cannon_CannonPowerChange")
-	cannonLeft.connect("CannonShoot", UIMain, "_on_Cannon_Shot")
-	if !is_connected("UIScoreChange", UIMain, "_on_UIScore_Change"):
-		var _res = connect("UIScoreChange", UIMain, "_on_UIScore_Change")
-	UIMain.connect("UIResetGame", self, "_on_UI_ResetGame")
-	UIMain.connect("UIdummyTargetTimerChange", Config, "_on_dummytarget_TimerChange")
-	var tmp = connect("CannonReset", cannonLeft, "_reset_CannonPower")
-	if (tmp) != OK:
-		print("Connect MainGame::_ready() -> Connect to CannonReset Fehlgeschlagen: %s" % tmp)
+#	cannonLeft.connect("CannonAngelChange", Preloads.UIMain, "_on_Cannon_CannonAngelChange")
+	cannonLeft.connect("CannonPowerChange", Preloads.UIMain, "_on_Cannon_CannonPowerChange")
+	cannonLeft.connect("CannonShoot", Preloads.UIMain, "_on_Cannon_Shot")
+#	if !is_connected("UIScoreChange", Preloads.UIMain, "_on_UIScore_Change"):
+#		var _res = connect("UIScoreChange", Preloads.UIMain, "_on_UIScore_Change")
+#	Preloads.UIMain.connect("UIResetGame", self, "_on_UI_ResetGame")
+#	Preloads.UIMain.connect("UIdummyTargetTimerChange", Config, "_on_dummytarget_TimerChange")
+#	var tmp = connect("CannonReset", Preloads.cannonLeft, "_reset_CannonPower")
+#	if (tmp) != OK:
+#		print("Connect MainGame::_ready() -> Connect to CannonReset Fehlgeschlagen: %s" % tmp)
 
-	Map.generate_world_tilemap_base()
+#	Preloads.Map.generate_world_tilemap_base()
 #	$Chunk.generate_world_block_base()
+	pass
+
+
+func _on_MainGame_ready() -> void:
+#	add_cannon_left()
+#	add_DummyTarget()
+	pass
 
 
 func add_cannon_left(pos:Vector2 = Vector2.INF):
 #	cannonLeft.position = TerrainLine.points[0]
 #	cannonLeft.position.x = TerrainLine.castlewidth / 2
 	if pos == Vector2.INF:
-		var p = Map.plattform[ceil($TerrainMap.plattform.size()/2)]
-		cannonLeft.position = Map.map_to_world(p)
+		var p = Preloads.Map.plattform[ceil(Preloads.Map.plattform.size()/2)]
+		cannonLeft.position = Preloads.Map.map_to_world(p)
 #		cannonLeft.position.x = Chunk.castlewidth / 2
-		PlayerLeft.add_child(cannonLeft)
+		Preloads.PlayerLeft.add_child(cannonLeft)
 	else:
 		cannonLeft.position = pos
-
-
-func add_DummyTarget_to_Line():
-	if Chunk.points.size() < 5:
-#	if TerrainLine.points.size() < 5:
-		pass
-
-	var tlSize = Chunk.world_tiles_x
-	var rmin : float = 2
-	var rmax = tlSize-1
-
-	randomize()
-
-	## Zufällige Position wählen
-	var i = rand_range(rmin, rmax)
-
-	target = Dummy.instance()
-	target.position = Chunk.points[int(i)]
-	target.score += int(i)
-	target.get_transform().scaled(Vector2(0.2,0.2))
-	target.autoDespawn = Config.config_data["Game"]["DummyTarget"]["TimerEnabled"]
-
-	if !target.is_connected("Hit", self, "_on_Dummy_Hited"):
-		target.connect("Hit", self, "_on_Dummy_Hited")
-
-	call_deferred("add_child", target)
 
 
 func add_DummyTarget():
 	randomize()
 
-	var rand = ceil(rand_range(Map.mod, Map.world_tiles_x))
-	var tiles = Map.get_used_cells_by_id(0)
+	var rand = ceil(rand_range(Preloads.Map.mod, Preloads.Map.world_tiles_x))
+	var tiles = Preloads.Map.get_used_cells_by_id(0)
 
 	var randtile = ceil(rand_range(0, tiles.size()-1))
 
-	target = Dummy.instance()
+	target = Preloads.DummyTarget.instance()
 	var tile_coord = tiles[ceil(randtile)]
-	var posi = Map.map_to_world(tile_coord)
+	var posi = Preloads.Map.map_to_world(tile_coord)
 	target.position = posi
 	target.score += int(randtile)
 #	target.get_transform().scaled(Vector2(1,1))
@@ -108,17 +73,13 @@ func add_DummyTarget():
 		target.connect("Hit", self, "_on_Dummy_Hited")
 
 	call_deferred("add_child", target)
-
-
-func _on_MainGame_ready() -> void:
-#	add_cannon_left()
-	add_DummyTarget()
-	pass
+	call_deferred("add_child", target)
 
 
 func _on_Dummy_Hited(score : int) -> void:
 	emit_signal("UIScoreChange", score)
 	add_DummyTarget()
+
 
 func _on_Ground_Hited() -> void:
 	emit_signal("UIScoreChange", -2)
@@ -129,23 +90,17 @@ func _on_UI_ResetGame() -> void:
 #	ToDo:
 #	Vor dem generieren bei Reset
 #	Erst die alten Blocks entfernen
-#	Chunk.generate_world()
-#	TerrainLine.init_line()
 	get_tree().call_group("Dummy", "queue_free")
 	get_tree().call_group("Shoots", "queue_free")
 	yield(get_tree(), "idle_frame")
 	emit_signal("CannonReset")
 	add_DummyTarget()
-#	cannonLeft.position = TerrainLine.points[0]
-#	cannonLeft.position.x = TerrainLine.castlewidth / 2
-#	var pos:Vector2 = Chunk.points[0]
-#	pos.x = Chunk.castlewidth / 2
 
 #	add_cannon_left(pos)
 
 
 func _on_Bullet_exploded(pos):
-	var e = Explosion.instance()
+	var e = Preloads.Explosion.instance()
 	add_child(e)
 	e.position = pos
 #	tank.can_shoot = true
@@ -174,12 +129,4 @@ func _on_Bullet_exploded(pos):
 #
 #	add_child(castleleft)
 #	add_child(castleright)
-#	pass
-
-
-#func _on_TerrainLine_ready() -> void:
-#	pass
-
-
-#func _on_TerrainLine_tree_entered() -> void:
 #	pass
