2133031f138e7f43fed4212cb7a896d5ef103c70
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-02-27 19:47:00 +0100

    Add: Signals die vom Bullet verwendet werden im ready connected

--- CannonBall/Cannonball.gd
+++ CannonBall/Cannonball.gd
@@ -1,7 +1,5 @@
 extends Area2D
 
-signal exploded
-signal GroundHit
 
 var velocity = Vector2.ZERO
 export var P1 = 1
@@ -9,10 +7,13 @@ onready var screensize = get_viewport().get_visible_rect().size
 onready var Main = $"/root/MainGame"
 
 var g = 150
+var Ply = ""
+
 
 func _ready() -> void:
-	var tmp = connect("GroundHit", Main, "_on_Ground_Hited")
-	print("Warning Connect: %s" % tmp)
+	SignalBus.connect("GroundHit", self, "_on_Ground_Hited")
+	SignalBus.connect("exploded", self, "_on_Bullet_exploded")
+	SignalBus.connect("UIScoreChange", Preloads.UIMain, "_on_UIScore_Change")
 
 
 func _process(delta):
@@ -21,16 +22,24 @@ func _process(delta):
 	position += velocity * delta
 
 	if position.y > screensize.y+100 or position.x > screensize.x+100:
-		emit_signal("GroundHit")
+		SignalBus.emit_signal("GroundHit")
 		queue_free()
 
 
 func _on_Cannonball_body_entered(body):
-	emit_signal("exploded", position + transform.x * 37)
+	SignalBus.emit_signal("exploded", position + transform.x * 37)
 	if body.is_in_group("Dummy") and body.has_method("_hit_ByBall"):
 		body._hit_ByBall()
 	if body.is_in_group("Ground"):
-		emit_signal("GroundHit")
+		SignalBus.emit_signal("GroundHit")
 	queue_free()
 
 
+func _on_Bullet_exploded(pos):
+	var e = Preloads.Explosion.instance()
+	add_child(e)
+	e.position = pos
+
+
+func _on_Ground_Hited() -> void:
+	SignalBus.emit_signal("UIScoreChange", -2)
