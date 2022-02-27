dc41087d37668e28b860702b11806db0d93a0161
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-02-27 22:40:21 +0100

    Add: Scne MainMenuUI

---
+++ UI/MainMenuUI.gd
@@ -0,0 +1,36 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_buttPlay_pressed():
+	# Change Scene to Game Scene
+	pass # Replace with function body.
+
+
+func _on_buttHightScore_pressed():
+	# Show Highscore Scene
+	pass # Replace with function body.
+
+
+func _on_buttSettings_pressed():
+	# Show Settings Scene
+	pass # Replace with function body.
+
+
+func _on_buttExit_pressed():
+	# Close Game, Back to System. Not on HTML
+	pass # Replace with function body.
