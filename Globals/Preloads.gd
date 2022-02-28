15defed58d7814b943659d7e6c16c22dd74d5730
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-02-28 23:01:26 +0100

    Fix: Reihenfolge der OnReady Zuweisung in globals ===: Ermitteln der Nodes aus der GameScene

--- Globals/Preloads.gd
+++ Globals/Preloads.gd
@@ -6,22 +6,23 @@ onready var Explosion:PackedScene = preload("res://CannonBall/Explosion.tscn")
 onready var DummyTarget:PackedScene = preload("res://DummyTarget/DummyTarget.tscn")
 onready var Bullet = preload("res://CannonBall/Cannonball.tscn")
 
-# Player Node's
-onready var PlayerRootNode = $"../MainGame/Players"
-onready var PlayerLeft = $"../MainGame/Players/Player1"
-onready var PlayerRight = $"../MainGame/Players/Player2"
-
-# UI
-onready var UIMain = $"/root/MainGame/InGameUI"
-
-# TileMap
-onready var Map = $"/root/MainGame/TerrainMap"
-
 # Main Game Scene
 onready var MainGame = preload("res://MainGame.tscn").instance()
 #onready var MainGameUI = preload("res://MainGame.tscn").instance()
 onready var MainGameScene = preload("res://MainGame.tscn")
 onready var MainMenuScene = preload("res://UI/MainMenuUI.tscn")
+
+# Player Node's
+onready var PlayerRootNode = MainGame.get_node("Players")
+onready var PlayerLeft = MainGame.find_node("Player1")
+onready var PlayerRight = MainGame.find_node("Player2")
+
+# UI
+onready var UIMain = MainGame.find_node("InGameUI")
+
+# TileMap
+onready var Map = MainGame.find_node("TerrainMap")
+
 
 func _ready():
 	# Preload the Assets
