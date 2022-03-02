1f32655be2e49fc4d5a027ade97bc72d77cf9667
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-03-02 23:06:07 +0100

    Chg: Collision detrectio to body exit Chg: Try One Wy Collision

--- CannonBall/Cannonball.gd
+++ CannonBall/Cannonball.gd
@@ -25,20 +25,31 @@ func _process(delta):
 		queue_free()
 
 
-func _on_Cannonball_body_entered(body):
-	SignalBus.emit_signal("exploded", position + transform.x * 37)
-	if body.is_in_group("Dummy") and body.has_method("_hit_ByBall"):
-		body._hit_ByBall()
-	if body.is_in_group("Ground"):
-		SignalBus.emit_signal("GroundHit")
-	queue_free()
+func _on_Cannonball_body_entered(_body):
+	pass
 
 
 func _on_Bullet_exploded(pos):
 	var e = Preloads.Explosion.instance()
 	Preloads.PlayerLeft.add_child(e)
 	e.position = pos
+#	yield(e, "animation_finished")
 
 
 func _on_Ground_Hited() -> void:
 	SignalBus.emit_signal("UIScoreChange", -2)
+
+
+func _on_Cannonball_body_exited(body):
+	$collisionShape2D.set_deferred("disabled", true)
+	SignalBus.emit_signal("exploded", position + transform.x * 37)
+#	yield(get_tree(), "idle_frame")
+	call_deferred("queue_free")
+	if body.is_in_group("Dummy") and body.has_method("_hit_ByBall"):
+		body._hit_ByBall()
+	if body.is_in_group("Ground"):
+		SignalBus.emit_signal("GroundHit")
+#	call_deferred("set", "disabled", "true")
+#	yield(get_tree(), "idle_frame")
+#	queue_free()
+	pass # Replace with function body.
