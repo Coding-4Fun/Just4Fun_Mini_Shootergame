09d8a8d0b510d3a6e6170a010fc4f6a7313ceda7
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-03-04 15:08:20 +0100

    Add: Zuweisung Dummy Group Chg: Dummy erzeugen wieder integriert Upd: Dummy Position um TileSize/2 erhöht damit diese Mittig erscheinen Add: Dummy der Dummy Gruppe untergeordnet Upd: Connect des Dummy Hit Signals um Assert erweitert Fix: Signam Emit für Score Zählung

--- MainGame.gd
+++ MainGame.gd
@@ -3,7 +3,6 @@ extends Node2D
 
 signal CannonReset
 
-
 onready var cannonLeft = Preloads.Cannon.instance()
 onready var cannonBarrelLeft = cannonLeft.get_node("Barrel")
 onready var cannonBarrelLeftMuzzle = cannonBarrelLeft.get_node("Muzzle")
@@ -11,9 +10,6 @@ onready var cannonBarrelLeftMuzzle = cannonBarrelLeft.get_node("Muzzle")
 #onready var cannonRight = cannon.instance()
 #onready var cannonBarrelRight = cannonRight.get_node("Barrel")
 #onready var cannonBarrelRightMuzzle = cannonBarrelRight.get_node("Muzzle")
-
-
-
 
 var target
 
@@ -44,6 +40,9 @@ func _enter_tree() -> void:
 
 	## UI
 	Preloads.UIMain = find_node("InGameUI")
+	
+	## Dummy Gruppen Node
+	Preloads.DummyTargetGroup = find_node("DummyTargets")
 
 	## TileMap
 	Preloads.Map = find_node("TerrainMap")
@@ -51,7 +50,7 @@ func _enter_tree() -> void:
 
 func _on_MainGame_ready() -> void:
 #	add_cannon_left()
-#	add_DummyTarget()
+	add_DummyTarget()
 	pass
 
 
@@ -73,26 +72,26 @@ func add_DummyTarget():
 #	var rand = ceil(rand_range(Preloads.Map.mod, Preloads.Map.world_tiles_x))
 	var tiles = Preloads.Map.get_used_cells_by_id(0)
 
-	var randtile = ceil(rand_range(0, tiles.size()-1))
+	var randtile = ceil(rand_range(10, tiles.size()-1))
 
 	target = Preloads.DummyTarget.instance()
 	var tile_coord = tiles[ceil(randtile)]
 	var posi = Preloads.Map.map_to_world(tile_coord)
+	posi.x += 8
 	target.position = posi
 	target.score += int(randtile)
 #	target.get_transform().scaled(Vector2(1,1))
 	target.autoDespawn = Config.config_data["Game"]["DummyTarget"]["TimerEnabled"]
 
 	if !target.is_connected("Hit", self, "_on_Dummy_Hited"):
-		target.connect("Hit", self, "_on_Dummy_Hited")
+		assert(target.connect("Hit", self, "_on_Dummy_Hited")==OK)
 
-	call_deferred("add_child", target)
-	call_deferred("add_child", target)
+	Preloads.DummyTargetGroup.add_child(target)
 
 
 func _on_Dummy_Hited(score : int) -> void:
-	emit_signal("UIScoreChange", score)
-#	add_DummyTarget()
+	SignalBus.emit_signal("UIScoreChange", score)
+	add_DummyTarget()
 
 
 func _on_UI_ResetGame() -> void:
