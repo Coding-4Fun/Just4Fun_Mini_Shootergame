1fb13d9cbcfd161abc84a8111dcc468f471a6756
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-03-04 10:26:26 +0100

    Chg: Nicht mehr benötigte Func und Kommentarzeilen entfernt Upd: Collisionscheck auf Tile ID 0

--- CannonBall/Cannonball.gd
+++ CannonBall/Cannonball.gd
@@ -19,15 +19,8 @@ func _ready() -> void:
 		assert(SignalBus.connect("exploded", self, "_on_Bullet_exploded", [], CONNECT_REFERENCE_COUNTED)==OK, "Fehler2")
 
 
-func _process(_delta):
-#	velocity.y += g * delta
-#	rotation = velocity.angle()
-#	position += velocity * delta
-#
-#	if position.y > screensize.y+100 or position.x > screensize.x+100:
-#		SignalBus.emit_signal("GroundHit")
-#		queue_free()
-	pass
+#func _process(_delta):
+#	pass
 
 
 func _physics_process(delta: float) -> void:
@@ -42,45 +35,18 @@ func _physics_process(delta: float) -> void:
 			tile_pos -= collision.normal
 			# Get the tile id
 			var tile_id = collision.collider.get_cellv(tile_pos)
-			print("collide: %s %s" % [name, tile_id])
-			if tile_id == 7:
-				collided = true
-				yield(get_tree(), "idle_frame")
+			if tile_id == 0:
 				SignalBus.emit_signal("exploded", position + transform.x * 37)
-				.emit_signal("GroundHit")
+				emit_signal("GroundHit")
 				$collisionShape2D.disabled = false
-#				print("collide: " + name)
 			call_deferred("queue_free")
-	pass
-
-
-func _on_Cannonball_body_entered(_body):
-	pass
 
 
 func _on_Bullet_exploded(pos):
 	var e = Preloads.Explosion.instance()
 	Preloads.PlayerLeft.add_child(e)
 	e.position = pos
-#	yield(e, "animation_finished")
 
 
 func _on_Ground_Hited() -> void:
-#	if is_queued_for_deletion():
-	print("GroundHit: " + name)
-	emit_signal("UIScoreChange", -2)
-
-
-func _on_Cannonball_body_exited(body):
-	$collisionShape2D.set_deferred("disabled", true)
-	SignalBus.emit_signal("exploded", position + transform.x * 37)
-#	yield(get_tree(), "idle_frame")
-	call_deferred("queue_free")
-	if body.is_in_group("Dummy") and body.has_method("_hit_ByBall"):
-		body._hit_ByBall()
-	if body.is_in_group("Ground"):
-		SignalBus.emit_signal("GroundHit")
-#	call_deferred("set", "disabled", "true")
-#	yield(get_tree(), "idle_frame")
-	queue_free()
-	pass # Replace with function body.
+	SignalBus.emit_signal("UIScoreChange", -2)
