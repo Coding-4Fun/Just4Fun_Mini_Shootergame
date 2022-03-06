7dc0372361050166b706bd99585baf1107aab83b
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-03-06 01:49:57 +0100

    Add: Laden der Werte für Conditions aus der Config

--- UI/Settings/MaxShotSlider.gd
+++ UI/Settings/MaxShotSlider.gd
@@ -2,17 +2,27 @@ extends Control
 
 onready var MaxShotLabel = $"labelMaxShots"
 onready var MaxShotSlider = $"MaxShotSlider"
+onready var MaxShotSwitchr = $"cButtSwitchMaxShots"
 
 func _ready():
 	MaxShotLabel.text = str(MaxShotSlider.value)
+	MaxShotSwitchr.pressed = Config.config_data["Game"]["Condition"]["MaxShotsEnabled"] 
+	MaxShotSlider.value = Config.config_data["Game"]["Condition"]["MaxShotsValue"]
 	pass
 
 
 func _on_MaxShotSlider_value_changed(value):
 	MaxShotLabel.text = str(value)
+	Config.config_data["Game"]["Condition"]["MaxShotsValue"] = value
 	pass # Replace with function body.
 
 
 func _on_cButtSwitchMaxShots_pressed():
 	$MaxShotSlider.editable = !$MaxShotSlider.editable
+	
+	pass # Replace with function body.
+
+
+func _on_cButtSwitchMaxShots_toggled(button_pressed):
+	Config.config_data["Game"]["Condition"]["MaxShotsEnabled"] = button_pressed
 	pass # Replace with function body.
