fd8d8e725d44397902a48f02e44724c66e4e8037
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-03-06 02:27:46 +0100

    Ren: Scipt umbenannt

--- UI/Settings/MaxShotSettings.gd
+++ UI/Settings/MaxShotSettings.gd
@@ -1,28 +1,24 @@
 extends Control
 
-onready var MaxShotLabel = $"labelMaxShots"
-onready var MaxShotSlider = $"MaxShotSlider"
-onready var MaxShotSwitchr = $"cButtSwitchMaxShots"
+onready var MaxShotLabel = $labelMaxShots
+onready var MaxShotSlider = $MaxShotSlider
+onready var MaxShotSwitch = $cButtSwitchMaxShots
 
 func _ready():
 	MaxShotLabel.text = str(MaxShotSlider.value)
-	MaxShotSwitchr.pressed = Config.config_data["Game"]["Condition"]["MaxShotsEnabled"] 
+	MaxShotSwitch.pressed = Config.config_data["Game"]["Condition"]["MaxShotsEnabled"] 
 	MaxShotSlider.value = Config.config_data["Game"]["Condition"]["MaxShotsValue"]
-	pass
+	MaxShotSlider.editable = Config.config_data["Game"]["Condition"]["MaxShotsEnabled"]
 
 
 func _on_MaxShotSlider_value_changed(value):
 	MaxShotLabel.text = str(value)
 	Config.config_data["Game"]["Condition"]["MaxShotsValue"] = value
-	pass # Replace with function body.
 
 
 func _on_cButtSwitchMaxShots_pressed():
 	$MaxShotSlider.editable = !$MaxShotSlider.editable
-	
-	pass # Replace with function body.
 
 
 func _on_cButtSwitchMaxShots_toggled(button_pressed):
 	Config.config_data["Game"]["Condition"]["MaxShotsEnabled"] = button_pressed
-	pass # Replace with function body.
