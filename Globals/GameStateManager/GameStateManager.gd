db2195ff24fea9b7317dfa94ee8c78cee2ae8620
Dirk Buck <dirk.buck@wolsdorff-tobacco.de>
2022-03-08 13:20:49 +0100

    Add: Einbinden des GameOver Dialog Add: Wechseln zur DialogsSzene

--- Globals/GameStateManager/GameStateManager.gd
+++ Globals/GameStateManager/GameStateManager.gd
@@ -6,6 +6,8 @@ export var _shots:int = 0
 
 signal GameStateChange
 
+#onready var GameOverDlg = preload("res://UI/GameEndDialog.tscn")
+
 func _ready():
 	if !is_connected("GameStateChange", self, "_on_GameStateChange"):
 		if connect("GameStateChange", self, "_on_GameStateChange"):
@@ -28,9 +30,10 @@ func _on_GameStateChange(newscore, newhits, newshots) -> void:
 func _check_GameWinCondition() -> void:
 	# Check auf Max Shots
 	if Config.config_data["Game"]["Condition"]["MaxShotsEnabled"]:
-		var maxshots = Config.config_data["Game"]["Condition"]["MaxShotsValue"] 
+		var maxshots = Config.config_data["Game"]["Condition"]["MaxShotsValue"]
 		if _shots >= maxshots:
 			print("Game Over. You have reached Max Shots")
+			_show_GameOverDialog()
 
 	# Check auf Score <> MinMaxScore
 	if Config.config_data["Game"]["Condition"]["MinMaxScoreEnabled"]:
@@ -38,8 +41,18 @@ func _check_GameWinCondition() -> void:
 		if scorevalue < 0:
 			if _score <= scorevalue:
 				print("Game Over. You LOOSE. You have reached the minmum Scores")
+				_show_GameOverDialog()
 		elif scorevalue > 0:
 			if _score >= scorevalue:
 				print("Game Over. You WON. You have reached the minmum Scores")
-	pass
+				_show_GameOverDialog()
 
+
+func _show_GameOverDialog(win:bool = false) -> void:
+	var go = Preloads.MainGameScene.instance()
+	if get_tree().change_scene_to(Preloads.GameOverScene) != OK:
+		print("Error: change_scene_to()::GameOver")
+	# if win == true:
+#		GameOverDialog-WinlooseLabel-text = You Win
+#	else
+#		GameOverDialog-WinlooseLabel-text = You Loose
