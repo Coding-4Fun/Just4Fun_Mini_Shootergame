2382c5e4b0257188cfb97a070e8becc41a0d2706
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-03-27 14:04:50 +0200

    Rollback: Godot zu Version 3.4.2
    Version 3.4.4 im Action Template noch nicht verfügbar

---
+++ cli.cmd
@@ -0,0 +1,2 @@
+P:\SteamLibrary\steamapps\common\Godot Engine>
+godot.windows.opt.tools.64.exe --verbose --export-debug "Windows Desktop" --test gui --path "P:\3. GODOT Projekte\Projekte\Just4Fun_Mini_Shootergame" "P:\3. GODOT Projekte\Projekte\Just4Fun_Mini_Shootergame\.Export\MiniShooter.exe" > "P:\3. GODOT Projekte\Projekte\Just4Fun_Mini_Shootergame\.export\export.txt"
