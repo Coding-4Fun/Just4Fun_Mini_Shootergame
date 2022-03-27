cf9d62ded6ddaa5a14818e83c486c80165cd60c2
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-03-27 23:16:53 +0200

    Add: Maximale Spielzeit für WinLoose Condition checken Add: Timer zum zählen der abgelaufenden ZEit

--- Globals/GameStateManager/GameStateManager.gd
+++ Globals/GameStateManager/GameStateManager.gd
@@ -6,6 +6,8 @@ export var _shots:int = 0
 
 var gameWin:int = -1
 var pm
+var GameTimer:Timer
+var GameTimerTimeElapsed:int = 0
 
 signal GameStateChange
 
@@ -15,8 +17,16 @@ func _ready():
 	if !is_connected("GameStateChange", self, "_on_GameStateChange"):
 		if connect("GameStateChange", self, "_on_GameStateChange"):
 			print("Error - GamestateManager: connect GameStateChange")
+			
+	GameTimer = Timer.new()
+	GameTimer.connect("timeout",self,"_on_timer_timeout") 
+	add_child(GameTimer)
 	pass
 
+
+func _on_timer_timeout() ->void:
+	GameTimerTimeElapsed += 1
+	pass
 
 func _on_GameStateChange(newscore, newhits, newshots) -> void:
 	_score = newscore
@@ -31,6 +41,13 @@ func _on_GameStateChange(newscore, newhits, newshots) -> void:
 #				"MinMaxScoreEnabled": false,
 #				"MinMaxScore": -1000
 func _check_GameWinCondition() -> void:
+	# Check Game Timeout
+	if Config.config_data["Game"]["Condition"]["MaxGameTimeEnabled"]:
+		var timeout = Config.config_data["Game"]["Condition"]["MaxGameTimeValue"]
+		if !GameTimer.is_stopped() and GameTimerTimeElapsed >= timeout:
+			print("Game Over. Your Game Time has left.")
+			_show_GameOverDialog(3)
+		
 	# Check auf Max Shots
 	if Config.config_data["Game"]["Condition"]["MaxShotsEnabled"]:
 		var maxshots = Config.config_data["Game"]["Condition"]["MaxShotsValue"]
