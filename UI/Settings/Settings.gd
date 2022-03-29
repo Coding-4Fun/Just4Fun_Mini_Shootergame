f06b6f6df800217ea5996673e33ee90d70fc18bc
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-03-29 13:15:45 +0200

    Chg: Settings UI in eine eigene Szene verschoben

---
+++ UI/Settings/Settings.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _ready():
+	pass
+
+func _on_buttBackToMenu_pressed() -> void:
+###	assert(get_tree().change_scene_to(Preloads.MainMenuScene) == OK, "Error: change_scene_to()::buttBackToMenu_pressed")
+	if !GSM.GameTimer.is_stopped():
+		GSM.GameTimer.stop()
+	if get_tree().change_scene_to(Preloads.MainMenuScene) != OK:
+		print("Error: change_scene_to()::buttBackToMenu_pressed")
+	Config.save_gameconfig()
