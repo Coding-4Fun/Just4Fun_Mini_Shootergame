461245a7dc4ee2b7daf93929a1a5e238414b1cbf
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-05-29 14:01:25 +0200

    Upd: Redesign der IngameHud UI
    Settings UI will follow

--- UI/InGameUI.gd
+++ UI/InGameUI.gd
@@ -9,8 +9,8 @@ var _hits : float = 0
 signal UIResetGame
 signal UIdummyTargetTimerChange
 
-@onready var OptionUI = $IngameUIBottom/vBoxContainer/vBoxSettings
-@onready var GameHudUI = $IngameUIBottom/vBoxContainer/hBoxHud
+@onready var OptionUI = $BoxContainer/vBoxSettings
+@onready var GameHudUI = $BoxContainer/hBoxHudLower
 
 func _ready() -> void:
 	if !SignalBus.is_connected("CannonAngelChange", self._on_Cannon_CannonAngelChange):
@@ -39,54 +39,55 @@ func _ready() -> void:
 			print("Error - InGameUI.gd: connect signal UIdummyTargetTimerChange")
 
 	var test = Preloads.PlayerLeft.find_child("Cannon")
-	$IngameUIBottom/vBoxContainer/hBoxHud/vBoxContainerShots/hBoxShots/labShots.text = str(_shots)
-	$IngameUIBottom/vBoxContainer/hBoxHud/vBoxContainerShots/hBoxHits/labHits.text = str(_hits)
-	$IngameUIBottom/vBoxContainer/hBoxHud/hBoxScore/labScore.text = str(_score)
-	$IngameUIBottom/vBoxContainer/hBoxHud/hBoxPower/labPower.text = str(test.min_velocity)
-	$IngameUIBottom/vBoxContainer/hBoxHud/vBoxContainerPoints/hBoxPointsPerShot/labPointsPerShots.text = "0.0"
 
-	$IngameUIBottom/vBoxContainer/vBoxSettings/vBoxSettings/hBoxOptions/buttSwitchTargetTimer.button_pressed = Config.config_data["Game"]["DummyTarget"]["TimerEnabled"]
+	$BoxContainer/hBoxHudLower/hBoxShots/labShots.text = str(_shots)
+	$BoxContainer/hBoxHudLower/hBoxHits/labHits.text = str(_hits)
+	$vBoxContainer/hBoxHud/hBoxScore/labScore.text = str(_score)
+	$BoxContainer/hBoxHud/hBoxPower/labPower.text = str(test.min_velocity)
+	$BoxContainer/hBoxHud/hBoxPointsPerShot/labPointsPerShots.text = "0.0"
 
-	GSM.GameTimeTextLabel = GameHudUI.get_node("vBoxContainerGameTimer/hBoxGametimer/labelGameTime")
+	$vBoxContainer/vBoxSettings/hBoxOptions/buttSwitchTargetTimer.button_pressed = Config.config_data["Game"]["DummyTarget"]["TimerEnabled"]
+
+	GSM.GameTimeTextLabel = GameHudUI.get_node("hBoxGametimer/labelGameTime")
 
 
 # Update UI Label Text when the shootpower changed
 func _on_Cannon_CannonAngelChange(newAngel : int) -> void:
-	$"IngameUIBottom/vBoxContainer/hBoxHud/vBoxContainerGameTimer/hBoxAngel/labAngel".text = str(newAngel)
+	$"vBoxContainer/hBoxHud/vBoxContainerGameTimer/hBoxAngel/labAngel".text = str(newAngel)
 
 
 # Update UI Label when the shootangel changed
 func _on_Cannon_CannonPowerChange(newPower : int) -> void:
-	$IngameUIBottom/vBoxContainer/hBoxHud/hBoxPower/labPower.text = str(newPower)
+	$vBoxContainer/hBoxHud/hBoxPower/labPower.text = str(newPower)
 
 
 func _on_Cannon_Shot() -> void:
 	_shots += 1
-	$IngameUIBottom/vBoxContainer/hBoxHud/vBoxContainerShots/hBoxShots/labShots.text = str(_shots)
+	$vBoxContainer/hBoxHud/vBoxContainerShots/hBoxShots/labShots.text = str(_shots)
 	GSM.emit_signal("GameStateChange", _score, _hits, _shots)
 
 
 func _on_UIScore_Change(score) -> void:
 	_score += score
-	$IngameUIBottom/vBoxContainer/hBoxHud/hBoxScore/labScore.text = str(_score)
+	$vBoxContainer/hBoxHud/hBoxScore/labScore.text = str(_score)
 	GSM.emit_signal("GameStateChange", _score, _hits, _shots)
 	if score > 0:
 		_hits += 1
-		$IngameUIBottom/vBoxContainer/hBoxHud/vBoxContainerShots/hBoxHits/labHits.text = str(_hits)
+		$vBoxContainer/hBoxHud/vBoxContainerShots/hBoxHits/labHits.text = str(_hits)
 		GSM.emit_signal("GameStateChange", _score, _hits, _shots)
 	if _shots != 0:
 		var pps:float = float(float(_score) / float(_shots))
-		$IngameUIBottom/vBoxContainer/hBoxHud/vBoxContainerPoints/hBoxPointsPerShot/labPointsPerShots.text = "%.3f" % pps
+		$vBoxContainer/hBoxHud/vBoxContainerPoints/hBoxPointsPerShot/labPointsPerShots.text = "%.3f" % pps
 
 ## Reset the Game
-func _on_button_pressed() -> void:
+func _on_buttGameReset_button_pressed() -> void:
 	_shots = 0.0
 	_score = 0.0
 	_hits = 0.0
-	$IngameUIBottom/vBoxContainer/hBoxHud/hBoxScore/labScore.text = str(_score)
-	$IngameUIBottom/vBoxContainer/hBoxHud/vBoxContainerShots/hBoxShots/labShots.text = str(_shots)
-	$IngameUIBottom/vBoxContainer/hBoxHud/vBoxContainerShots/hBoxHits/labHits.text = str(_hits)
-	$IngameUIBottom/vBoxContainer/hBoxHud/vBoxContainerPoints/hBoxPointsPerShot/labPointsPerShots.text = "0.0"
+	$vBoxContainer/hBoxHud/hBoxScore/labScore.text = str(_score)
+	$vBoxContainer/hBoxHud/vBoxContainerShots/hBoxShots/labShots.text = str(_shots)
+	$vBoxContainer/hBoxHud/vBoxContainerShots/hBoxHits/labHits.text = str(_hits)
+	$vBoxContainer/hBoxHud/vBoxContainerPoints/hBoxPointsPerShot/labPointsPerShots.text = "0.0"
 	emit_signal("UIResetGame")
 
 # Öffnen und schliessen der Settings
@@ -96,8 +97,8 @@ func _on_buttGameOptions_pressed() -> void:
 	if Config.config_data["Game"]["Condition"]["MaxGameTimeEnabled"] == true and OptionUI.visible == false:
 		GSM.GameTimer.start()
 
-	$"IngameUIBottom/vBoxContainer/hBoxHud/vBoxContainerGameTimer/hBoxGametimer".visible = !GSM.GameTimer.is_stopped()
+	$"vBoxContainer/hBoxHud/vBoxContainerGameTimer/hBoxGametimer".visible = !GSM.GameTimer.is_stopped()
 
 
 func _on_buttSwitchTargetTimer_pressed() -> void:
-	emit_signal("UIdummyTargetTimerChange", $IngameUIBottom/vBoxContainer/vBoxSettings/hBoxOptions/buttSwitchTargetTimer.pressed)
+	emit_signal("UIdummyTargetTimerChange", $vBoxContainer/vBoxSettings/hBoxOptions/buttSwitchTargetTimer.pressed)
