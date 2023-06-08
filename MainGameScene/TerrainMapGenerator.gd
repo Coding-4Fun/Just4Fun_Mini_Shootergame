2649d7993818f00bed149247805c8b2a9670ca10
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-06-08 17:37:53 +0200

    Chg: Random Tile Function to Integer Func

--- MainGameScene/TerrainMapGenerator.gd
+++ MainGameScene/TerrainMapGenerator.gd
@@ -2,6 +2,8 @@ extends TileMap
 
 var tile_size:Vector2 = Vector2i(16,16) #.ceil()
 
+
+
 @onready var screensize:Vector2 = get_viewport().get_visible_rect().size
 @onready var midrange:float = screensize.y
 @onready var displacement := ceili((midrange / tile_size.y) / 2)
@@ -63,12 +65,14 @@ func generate_world_tilemap_base() -> void:
 			# Unterhalb der Graslinie
 			if y > current_displacement and y < world_tiles_y-2:
 				var rdm = randi_range(1,5)
-				rdm = ceil(rdm)
-				rdm = int(rdm)
+				rdm = ceili(rdm)
+#				rdm = int(rdm)
 				set_cell(0, Vector2i(x,y), rdm, Vector2i(0, 0))
 			elif y >= world_tiles_y-2:
 				set_cell(0, Vector2i(x,y), 7, Vector2i(0, 0))
 			# End Y Loop
+		
 
+			
 		var change_displacement = current_displacement + sign(randi_range(-5, 5))# pow(-1.0, randi() % 2)
 		current_displacement = change_displacement
