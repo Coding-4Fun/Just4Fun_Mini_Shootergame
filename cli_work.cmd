ebcfd8ff55828b89f7ec78496f34c74ac886196c
Fox-Alpha <fox-alpha@tarnhoerner.de>
2024-08-08 16:17:49 +0200

    Ref: Rework cli batch for cli exports

--- cli_work.cmd
+++ cli_work.cmd
@@ -1,3 +1,44 @@
 @echo off
-cd C:\Proggen\Godot\Godot_v3.5.1-stable_win64
-godot.windows.opt.tools.64.exe --verbose --export-debug "Windows Desktop" --test gui --path "C:\Proggen\Godot\Projekte\RTS_Dummy_Prototype" "C:\Proggen\Godot\Projekte\Export\RTS_Dummy_Prototype\RTS_Dummy_Prototype.exe" > "C:\Proggen\Godot\Projekte\Export\RTS_Dummy_Prototype\export.log.txt"
+chcp 65001
+
+cls
+
+REM Full Project Path
+set "project=%~dp0"
+REM Path to Godot Executable
+set "gdpath=C:\Proggen\Godot\Godot_v4.3-rc"
+set "godotexe=Godot_v4.3-rc2_win64.exe"
+REM Full Engine Path
+set "build_godot=%gdpath%\%godotexe%"
+
+REM Profilename in export_presets.cfg
+REM set "build_profile=Windows Desktop"
+set "build_profile=Windows_Pack_UI"
+REM export Type export-debug or export-release or export-pack (musst be .pck or .zip)
+REM set "build_type=export-debug"
+REM set "build_type=export-release"
+set "build_type=export-pack"
+
+
+REM Path to Export root folder
+set "build_path=C:\Proggen\Godot\Projekte\Just4Fun_Mini_Shootergame\4.1.x-Dev\export\"
+REM Subfolder for Export Profile, overwrites preset
+set "build_folder=%build_path%%build_profile%"
+
+REM Output binary name
+set "build_bin=MiniShooterGame.pck"
+REM Full Build Path+Name
+set "build_project=%build_folder%/%build_bin%"
+REM Full Build Log output
+set "build_log=%build_folder%\export.log.txt"
+
+echo %project%
+echo .
+
+REM Execute Build Process from export_presets.cfg
+"%build_godot%" --import --verbose --headless  --log-file "%build_log%"
+"%build_godot%" --verbose --%build_type% "%build_profile%" --headless  --log-file "%build_log%" --path %project% %build_project%
+
+REM Execute Build Process overwrite export_presets.cfg
+REM "%build_godot%" --verbose --%build_type% "%build_profile%" --headless  --log-file "%build_log%" --path "%project%" "%build_project%"
+
