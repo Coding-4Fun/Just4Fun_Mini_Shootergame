af09dd0b10f8b49bc42bfd5dfa38a9fe3866bca4
Fox-Alpha <fox-alpha@tarnhoerner.de>
2024-08-08 22:41:37 +0200

    Ref: Rework cli batch for cli exports for home dev Fix: backslashes in paths ===: encapsulate with "

--- cli.cmd
+++ cli.cmd
@@ -4,7 +4,7 @@ chcp 65001
 cls
 
 REM Full Project Path
-set "project=%~dp0"
+set "project=%~dp0\"
 REM Path to Godot Executable
 set "gdpath=P:\0. DevProgramme\Godot\Godot_v4.3-rc
 set "godotexe=Godot_v4.3-rc3_win64.exe"
@@ -37,8 +37,9 @@ echo .
 
 REM Execute Build Process from export_presets.cfg
 "%build_godot%" --import --verbose --headless  --log-file "%build_log%"
-"%build_godot%" --verbose --%build_type% "%build_profile%" --headless  --log-file "%build_log%" --path %project% %build_project%
-
+"%build_godot%" --verbose --headless --"%build_type%" "%build_profile%"  --log-file "%build_log%" --path "%project%" "%build_project%"
+REM echo --verbose --"%build_type%" "%build_profile%" --headless  --log-file "%build_log%" --path \"%project%\" "%build_project%"
+REM echo "P:\3. GODOT Projekte\Projekte\Just4Fun_Mini_Shootergame-Projekt\.Export\Windows_Pack_UI\export.log.txt" --path "P:\3. GODOT Projekte\Projekte\Just4Fun_Mini_Shootergame-Projekt\4.3\" "P:\3. GODOT Projekte\Projekte\Just4Fun_Mini_Shootergame-Projekt\.Export\Windows_Pack_UI/MiniShooterGame.pck"
 REM Execute Build Process overwrite export_presets.cfg
-REM "%build_godot%" --verbose --%build_type% "%build_profile%" --headless  --log-file "%build_log%" --path "%project%" "%build_project%"
+REM "%build_godot%" --verbose --%build_type% "%build_profile%" --headless  --log-file "%build_log%" --path "%project%\" "%build_project%"
 
