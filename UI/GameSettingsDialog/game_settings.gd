115d7590699c566c557700b663d237e6fd4bf4c3
Fox-Alpha <fox-alpha@tarnhoerner.de>
2024-08-14 22:33:51 +0200

    Add: Function for enable Dummy Timer Add: Function to Slieder change Dummy Time

--- UI/GameSettingsDialog/game_settings.gd
+++ UI/GameSettingsDialog/game_settings.gd
@@ -20,3 +20,15 @@ func _on_butt_start_game_pressed() -> void:
 func _on_butt_back_to_menu_pressed() -> void:
 	if get_tree().change_scene_to_packed(Preloads.MainMenuScene) != OK:
 		print("Error: change_scene_to()::buttBackToMenu_pressed")
+
+
+func _on_c_butt_switch_max_game_time_toggled(toggled_on: bool) -> void:
+	$centerContainer/vBoxContainer/hBoxContainerMaxGameTimer/MaxGameTimeSlider.editable = toggled_on
+	Config.ConfigValueChange.emit("TimerEnabled", toggled_on, "Game", "DummyTarget")
+	pass # Replace with function body.
+
+
+func _on_max_game_time_slider_drag_ended(value_changed: bool) -> void:
+	if value_changed:
+		var value = $centerContainer/vBoxContainer/hBoxContainerMaxGameTimer/MaxGameTimeSlider.value
+		Config.ConfigValueChange.emit("TimerCountdown", value, "Game", "TimerEnabled")
