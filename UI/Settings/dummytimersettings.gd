0e1b7244371d7dc5f0bce3ed457c5c1e38022347
Fox-Alpha <fox-alpha@tarnhoerner.de>
2024-08-15 11:06:24 +0200

    Chg: UI Signal connect per code ===: DummyTargetTimer Add: Script for UI Signals

--- UI/Settings/dummytimersettings.gd
+++ UI/Settings/dummytimersettings.gd
@@ -1,27 +1,33 @@
 extends Control
 
 
-@onready var MaxShotLabel : Label = $labelMaxShots
-@onready var MaxShotSlider : HSlider = $MaxShotSlider
-@onready var DummyTargetTimerSwitch : CheckButton = $cButtSwitchTargetTimer
+@onready var DummyTargetTimeLabel : Label = $hBoxTargetTimer/labelMaxGameTime
+@onready var DummyTargetTimerSwitch : CheckButton = $hBoxTargetTimer/cButtSwitchTargetTimer
+@onready var DummyTargetTimerSlider : HSlider = $hBoxTargetTimer/MaxGameTimeSlider
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	DummyTargetTimerSwitch.toggled.connect(_on_c_butt_switch_target_timer_toggled)
+	DummyTargetTimerSlider.drag_ended.connect(_on_max_game_time_slider_drag_ended)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+#func _process(delta: float) -> void:
+	#pass
+
+
 #buttSwitchTargetTimer
 func _on_c_butt_switch_target_timer_toggled(toggled_on: bool) -> void:
-	$centerContainer/vBoxContainer/hBoxContainerMaxGameTimer/MaxGameTimeSlider.editable = toggled_on
+	DummyTargetTimerSlider.editable = toggled_on
 	Config.ConfigValueChanged.emit("TimerEnabled", toggled_on, "Game", "DummyTarget")
 	pass # Replace with function body.
 
 
 func _on_max_game_time_slider_drag_ended(value_changed: bool) -> void:
 	if value_changed:
-		var value = $centerContainer/vBoxContainer/hBoxContainerMaxGameTimer/MaxGameTimeSlider.value
+		var value = DummyTargetTimerSlider.value
+		var minsek:String = "%02.0f" % [int(value) % 60]
+		DummyTargetTimeLabel.text = str(minsek)
 		Config.ConfigValueChanged.emit("TimerCountdown", value, "Game", "DummyTarget")
