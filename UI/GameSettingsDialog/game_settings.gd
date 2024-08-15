97fa83a698aab109671650658dd2274fea266aa8
Fox-Alpha <fox-alpha@tarnhoerner.de>
2024-08-15 08:25:40 +0200

    Fix: Signal Name Fix: Signal Parameter Values

--- UI/GameSettingsDialog/game_settings.gd
+++ UI/GameSettingsDialog/game_settings.gd
@@ -24,11 +24,11 @@ func _on_butt_back_to_menu_pressed() -> void:
 
 func _on_c_butt_switch_max_game_time_toggled(toggled_on: bool) -> void:
 	$centerContainer/vBoxContainer/hBoxContainerMaxGameTimer/MaxGameTimeSlider.editable = toggled_on
-	Config.ConfigValueChange.emit("TimerEnabled", toggled_on, "Game", "DummyTarget")
+	Config.ConfigValueChanged.emit("TimerEnabled", toggled_on, "Game", "DummyTarget")
 	pass # Replace with function body.
 
 
 func _on_max_game_time_slider_drag_ended(value_changed: bool) -> void:
 	if value_changed:
 		var value = $centerContainer/vBoxContainer/hBoxContainerMaxGameTimer/MaxGameTimeSlider.value
-		Config.ConfigValueChange.emit("TimerCountdown", value, "Game", "TimerEnabled")
+		Config.ConfigValueChanged.emit("TimerCountdown", value, "Game", "DummyTarget")
