77ec7e256640ea647825c81c37dabea0fc8beb61
Fox-Alpha <fox-alpha@tarnhoerner.de>
2024-08-15 09:41:08 +0200

    Chg: Button Pressed Signal connect per code

--- UI/GameSettingsDialog/game_settings.gd
+++ UI/GameSettingsDialog/game_settings.gd
@@ -1,8 +1,15 @@
 extends Control
 
 
+@onready var butt_start_game: Button = $centerContainer/vBoxContainer/hBoxButtons/buttStartGame
+@onready var butt_back_to_menu: Button = $centerContainer/vBoxContainer/hBoxButtons/buttBackToMenu
+
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	butt_start_game.pressed.connect(_on_butt_start_game_pressed)
+	butt_back_to_menu.pressed.connect(_on_butt_back_to_menu_pressed)
 	pass # Replace with function body.
 
 
