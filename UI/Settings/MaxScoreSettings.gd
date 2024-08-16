fc28a6cdbc0bf3844cae1466e63b246eca9820ff
Fox-Alpha <fox-alpha@tarnhoerner.de>
2024-08-16 09:00:03 +0200

    Fix: Initial Display for MaxScore Fix: Display on Change Fix: Save changed value

--- UI/Settings/MaxScoreSettings.gd
+++ UI/Settings/MaxScoreSettings.gd
@@ -5,28 +5,24 @@ extends Control
 @onready var MaxScoreSwitch : CheckButton = $cButtSwitchMaxScore
 
 func _ready():
-	MaxScoreLabel.text = str(MaxScoreSlider.value)
 	MaxScoreSwitch.button_pressed = Config.get_configdata_value("GameConditionMinMaxScoreEnabled")
 	MaxScoreSlider.value = Config.get_configdata_value("GameConditionMinMaxScoreValue")
 	MaxScoreSlider.editable = Config.get_configdata_value("GameConditionMinMaxScoreEnabled")
-	
-	var me : Variant
-	
-	
+	MaxScoreLabel.text = str(MaxScoreSlider.value)
 	
 	MaxScoreSwitch.toggled.connect(_on_cButtSwitchMaxScore_toggled)
 	MaxScoreSlider.drag_ended.connect(_on_MaxScoreSlider_drag_ended)
-	pass
 
 
 func _on_cButtSwitchMaxScore_toggled(toggled_on: bool):
 	MaxScoreSlider.editable = toggled_on
-	Config.ConfigValueChanged.emit("MinMaxScoreEnabled", toggled_on, "Game", "DummyTarget")
+	Config.ConfigValueChanged.emit("MinMaxScoreEnabled", toggled_on, "Game", "Condition")
 
 
 func _on_MaxScoreSlider_drag_ended(value_changed: bool) -> void:
 	if value_changed:
 		var value = MaxScoreSlider.value
-		var minsek:String = "%02.0f" % [int(value) % 60]
-		MaxScoreLabel.text = str(minsek)
+		#var minsek:String = "%02.0f" % [int(value) % 60]
+		#MaxScoreLabel.text = str(minsek)
+		MaxScoreLabel.text = str(value)
 		Config.ConfigValueChanged.emit("MinMaxScoreValue", value, "Game", "Condition")
