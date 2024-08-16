593cedb72952e007b81e4766bd29bb23dc8d6f6c
Fox-Alpha <fox-alpha@tarnhoerner.de>
2024-08-16 15:22:32 +0200

    TryFix: Playing sounds on click ===: No signals nor _ready() received by script

--- UI/components/sound_button/sound_button.gd
+++ UI/components/sound_button/sound_button.gd
@@ -2,8 +2,13 @@ extends Button
 
 
 func _ready():
-	pressed.connect(on_pressed)
+	#pressed.connect(on_pressed)
+	pass
 
 
 func on_pressed():
+	pass # Replace with function body.
+
+
+func _on_pressed() -> void:
 	$RandomStreamPlayerComponent.play_random()
