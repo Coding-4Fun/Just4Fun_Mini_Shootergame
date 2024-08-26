13f8ee811d8b3f8ededf31266bf7b4db0d0eb4db
Fox-Alpha <fox-alpha@tarnhoerner.de>
2024-08-26 16:13:28 +0200

    Add: empty func for placing the cannon

--- MainGameScene/terrain_layer/layer_cannon.gd
+++ MainGameScene/terrain_layer/layer_cannon.gd
@@ -3,4 +3,10 @@ extends TileMapLayerBase
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	super()
 	pass # Replace with function body.
+
+
+## Placing the Cannon inside the Castle
+func _on_MapGenerator_PlaceCannon(pattern : Vector2i) -> void:
+	pass
