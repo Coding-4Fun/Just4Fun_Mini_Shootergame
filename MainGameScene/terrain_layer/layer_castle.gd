27703e3e9910aabd9e3385dde2e78d5247a401e2
Fox-Alpha <fox-alpha@tarnhoerner.de>
2024-08-26 14:27:54 +0200

    Add: Setting Castle Pattern at right position

--- MainGameScene/terrain_layer/layer_castle.gd
+++ MainGameScene/terrain_layer/layer_castle.gd
@@ -5,9 +5,20 @@ extends TileMapLayerBase
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	SignalBus.MapGenerateWorldTileMap.connect(_on_MapGeneratorWorldTileMap)
+	super()
 	pass # Replace with function body.
 
 
-func _on_MapGeneratorWorldTileMap() -> void:
+func _on_MapGenerator_GenerateTerrain() -> void:
+	pass
+
+
+func _on_MapGenerator_PlaceCastle(pos : Vector2i) -> void:
 	clear()
+	print("CastleLayer_Place signal")
+	var pattern_count = tile_set.get_patterns_count()
+	var pattern :TileMapPattern = tile_set.get_pattern(0)
+	var pattern_size = pattern.get_size()
+	pos.y -= pattern_size.y
+	set_pattern(pos, pattern)	
+	pass
