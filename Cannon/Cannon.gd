94f2bbd18ee8d56088342ae1f1c134525d65fb17
Fox-Alpha <fox-alpha@tarnhoerner.de>
2025-02-25 10:04:03 +0100

    Add: Floating Text counter for reloading

--- Cannon/Cannon.gd
+++ Cannon/Cannon.gd
@@ -6,13 +6,16 @@ extends Node2D
 @export var min_velocity = 10000
 @export var max_velocity = 60000
 @export var gravity = 250
+@export var cooldown_tick := 3
 
-var can_shoot = true
-var current_rotation = 0
+var cooldown := 3
+var can_shoot := true
+var current_rotation := 0
 
 @onready var Muzzle = get_node("Barrel/Muzzle")
 @onready var Barrel = get_node("Barrel")
 @onready var coolDown = get_node("CoolDown")
+@onready var cooldowntimer: Timer = $Timer
 
 
 func _ready():
@@ -25,6 +28,9 @@ func _ready():
 			print("Error - Cannon.gd: connect signal CannonShooting")
 	
 	SignalBus.MapGeneratorPlaceCannon.connect(_on_MapGenerator_PlaceCannon)
+	
+	coolDown.max_value = 60.0 * cooldown
+	cooldown_tick = cooldown
 
 
 func _unhandled_input(event):
@@ -33,6 +39,8 @@ func _unhandled_input(event):
 	if event.is_action_released("cannon_shoot") and can_shoot:
 		SignalBus.CannonShooting.emit(Muzzle.global_transform, muzzle_velocity, gravity)
 		SignalBus.CannonShoot.emit()
+		SignalBus.FloatingText.emit(str(cooldown_tick), global_position)
+		cooldowntimer.start()
 		can_shoot = false
 	if event.is_action_released("cannon_power_plus"):
 		if Input.is_key_pressed(KEY_CTRL):
@@ -62,10 +70,13 @@ func _process(_delta):
 
 	if !can_shoot:
 		coolDown.value += 1+_delta
+		
 
-	if coolDown.value >= 100:
+	if coolDown.value >= coolDown.max_value:
 		can_shoot = true
 		coolDown.value = 0
+		cooldowntimer.stop()
+		cooldown_tick = cooldown
 		SignalBus.FloatingText.emit("RELOADED", global_position)
 
 
@@ -82,3 +93,9 @@ func _on_Cannon_ready():
 func _on_MapGenerator_PlaceCannon(pos : Vector2) -> void:
 	global_position = pos
 	pass
+
+
+func _on_cooldowntimer_timeout() -> void:
+	cooldown_tick -= 1
+	if cooldown_tick > 0 :
+		SignalBus.FloatingText.emit(str(cooldown_tick), global_position)
