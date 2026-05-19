dba5fc2ff1887089782fc16ddc971617c54cd91d
Fox-Alpha <fox-alpha@tarnhoerner.de>
2026-05-19 16:32:57 +0200

    Upd: Formated and linted Chg: replace filepath with UID for preloads

--- scripts/globals/preloads.gd
+++ scripts/globals/preloads.gd
@@ -1,41 +1,39 @@
 extends Node
 
-# Preload the Assets
-@onready var Cannon:PackedScene = preload("res://Cannon/Cannon.tscn")
-@onready var Explosion:PackedScene = preload("res://CannonBall/Explosion.tscn")
-@onready var DummyTarget:PackedScene = preload("res://DummyTarget/DummyTarget.tscn")
-@onready var PauseMenu:PackedScene = preload("res://UI/Pause/PauseMenu.tscn")
-@onready var Bullet :PackedScene = preload("res://CannonBall/Cannonball.tscn")
-@onready var floating_text_scene :PackedScene = preload("res://UI/components/floating_text/floating_text.tscn")
-
-# Main Game Scene
-@onready var MainGameScene :PackedScene = load("res://MainGameScene/MainGame.tscn")
-@onready var MainMenuScene :PackedScene = load("res://UI/MainMenuUI/MainMenuUI.tscn")
-@onready var GameSettingsScene :PackedScene = preload("res://UI/GameSettingsDialog/GameSettings.tscn")
-@onready var GameOverScene :PackedScene = preload("res://UI/GameEndDialog/GameEndDialog.tscn")
-
-@onready var rng : RandomNumberGenerator = RandomNumberGenerator.new()
-
 # Player Node's
-var PlayerRootNode:Node2D
-var PlayerLeft:Node2D
-var PlayerRight:Node2D
-var PlayerShots:Node2D
+var PlayerRootNode: Node2D
+var PlayerLeft: Node2D
+var PlayerRight: Node2D
+var PlayerShots: Node2D
 #
 ##
-var Main:Node2D
+var Main: Node2D
 #
 ## UI
-var UIMain:Control
-#
-## Dummys
-@onready var DummyTargetGroup:Node2D
-
+var UIMain: Control
 #
 ## TileMapLayer's
 var TerrainLayer: TileMapLayerBase
-var CannonLayer:  TileMapLayerBase
-var CastleLayer:  TileMapLayerBase
+var CannonLayer: TileMapLayerBase
+var CastleLayer: TileMapLayerBase
+
+# Preload the Assets
+@onready var Cannon: PackedScene = preload("uid://lci12xbkaqx2") ## Cannon Object
+@onready var DummyTarget: PackedScene = preload("uid://dwpyhrww6jkcv") ## DummyTarget Object
+@onready var Bullet: PackedScene = preload("uid://bkuw5djpqvu7i") ## CannonBall Object
+# Pre-Load Components
+@onready var floating_text_scene: PackedScene = preload("uid://cw7aupmlhq8xo")
+@onready var Explosion: PackedScene = preload("uid://xx6yx0io3fgf") ## Explosion Animation Node
+# Main Game Scene
+@onready var MainGameScene: PackedScene = load("uid://h8vcng8ps4vd")
+@onready var MainMenuScene: PackedScene = load("uid://b160gc30aq28t")
+@onready var GameSettingsScene: PackedScene = preload("uid://bdg32fb7rseev")
+@onready var PauseMenu: PackedScene = preload("uid://cujr5miucacq5")
+@onready var GameOverScene: PackedScene = preload("uid://rd0b5nysx6vq")
+@onready var rng: RandomNumberGenerator = RandomNumberGenerator.new()
+#
+## Dummys
+@onready var DummyTargetGroup: Node2D
 
 
 func _ready():
