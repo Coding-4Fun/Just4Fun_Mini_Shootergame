a2a60a07e38f47c8d3c123d472652d1fc301c666
Fox-Alpha <fox-alpha@tarnhoerner.de>
2026-05-19 16:32:14 +0200

    Chg: Formating and partly linted

--- scripts/players.gd
+++ scripts/players.gd
@@ -5,12 +5,13 @@ extends Node2D
 func _ready():
 	pass # Replace with function body.
 
-func _on_Player_Shoot(pos:Transform2D, velocity, gravity) -> void:
+
+func _on_Player_Shoot(pos: Transform2D, velocity, gravity) -> void:
 	#randomize()
-	var bullet : CharacterBody2D = Preloads.Bullet.instantiate()
+	var bullet: CharacterBody2D = Preloads.Bullet.instantiate()
 	bullet.Ply = name
 	bullet.name = str(randi())
-	bullet.transform = pos 
+	bullet.transform = pos
 	bullet.velocity = bullet.transform.x * velocity # muzzle_velocity
 	bullet.g = gravity
 	Preloads.PlayerShots.add_child(bullet)
